97ab28129a6f35beeb28e86d405956d29f1b79fe
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-10-08 12:46:50 +0100

    Formik hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  validate = ({ email, password, password2 }) => {
+    let errors = {};
+    if (!email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+    if (password.length < 8) {
+      errors.password = "Must be at least 8 characters";
+    }
+    if (password !== password2) {
+      errors.password2 = "Passwords do not match";
+    }
+    return errors;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Formik
+          initialValues={{ email: "", password: "", password2: "" }}
+          validate={this.validate}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {({ isSubmitting }) => (
+            <Form className="main-form">
+              <div className="group">
+                <label>Email</label>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" className="error" />
+              </div>
+              <div class="group">
+                <label>Password</label>
+                <Field type="password" name="password" />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="error"
+                />
+              </div>
+              <div class="group">
+                <label>Repeat Password</label>
+                <Field type="password" name="password2" />
+                <ErrorMessage
+                  name="password2"
+                  component="div"
+                  className="error"
+                />
+              </div>
+              <div class="buttons">
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
 }
 
 export default App;
